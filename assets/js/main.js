5687a162d1418df0394cbb1c32610d43e2a520ce
Tdogb <tdogbeard@gmail.com>
2022-09-25 14:43:15 -0400

    big update

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@
    */
   const burgerMenu = select('.burger')
   on('click', '.burger', function(e) {
-    burgerMenu.classList.toggle('active');
+
   })
 
   /**
@@ -108,18 +108,68 @@
     })
   });
 
-  function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    if(i==n) {
-      slides[i].style.display = "block";
-    } else  {
-      slides[i].style.display = "none";
+})()
 
+function nextSlide(num) {
+  let es = document.getElementsByClassName("display-image-div");
+  // es = document.getElementsByClassName("display-image");
+  for (i=0; i<es.length;i++) {
+    if(i != es){
+      es[i].style.display="none";
     }
   }
-  // slides[slideIndex-1].style.display = "block";
+  es[num].style.display = "block";
 }
 
-})()
+function open_iframe(file) {
+  // opened_page = file;
+  let bod = document.getElementById("index_body");
+  bod.setAttribute('scroll','no');
+  bod.setAttribute('style','overflow:hidden;');
+  let inactive_frames = bod.getElementsByClassName('inactive-frame');
+  for(i=0;i<inactive_frames.length;i++) {
+    let src_str = inactive_frames[i].src
+    let ind = src_str.lastIndexOf("/");
+    if(src_str.substring(ind+1)===file) {
+      inactive_frames[i].setAttribute('style','display:block;');
+      inactive_frames[i].setAttribute('class','active-frame')
+    }
+  }
+ 
+}
+
+function close_iframe() {
+    // burgerMenu.classList.toggle('active');
+    let bod = parent.document.getElementById("index_body");
+    console.log(bod)
+    bod.removeAttribute('scroll');
+    bod.setAttribute('style','overflow:auto;');
+    let active_frames = parent.document.getElementsByClassName('active-frame');
+    active_frames[0].setAttribute('style','display:none;');
+    active_frames[0].setAttribute('class','inactive-frame')
+}
+
+//Creates the list of images to click
+window.onload = function() {
+  // console.log("active");
+  var imgs = document.getElementsByClassName("display-image");
+  // var lis = document.getElementsByClassName("image-selector-li");
+  var ol = document.getElementsByClassName("image-selector");
+  for(i = 0; i < imgs.length;i++) {
+    const li_node = document.createElement("li");
+    li_node.setAttribute('class', 'image-selector-li');
+    const img_node = document.createElement("img");
+    var img_src = imgs[i].src;
+    let ind = img_src.lastIndexOf("/");
+    var img_new = img_src.substring(0,ind) + "/small-img" + img_src.substring(ind);
+    img_node.setAttribute('onload','this.width = this.naturalWidth; this.height = this.naturalHeight');
+    img_node.setAttribute('src',img_new);
+    img_node.setAttribute('onclick',"nextSlide("+i+")");
+    img_node.setAttribute('class','flex-active slide_selector');
+    img_node.setAttribute('draggable','false');
+    img_node.setAttribute('width','100');
+    img_node.setAttribute('height','100');
+    li_node.appendChild(img_node)
+    ol[0].appendChild(li_node);
+  }
+}
